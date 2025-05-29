c45f9c4cdf6edd531c558e60f8bc41b54b2a592d
Michael Yang <git@mxy.ng>
2025-05-29 12:11:26 -0700

    relpath

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
 		options = { theme = 'auto' },
 		sections = {
 			lualine_a = { { 'mode', fmt = function(str) return str:sub(1, 1) end } },
-			lualine_c = { { 'filename', path = 4 } },
+			lualine_c = { { 'filename', path = 1 } },
 		},
 		inactive_sections = {
 			lualine_c = { { 'filename', path = 1 } },
